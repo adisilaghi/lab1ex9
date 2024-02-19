ee88510ff9878765846b456fc2584319bb9cb7a3
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-02-19 22:23:57 +0200

    Add project files.

---
+++ lab1ex9/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Scrieti un program care interschimba valoarea a doua variabile intregi.*/
+
+Console.WriteLine("Introduceti Value1");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introduceti Value2");
+int y = Convert.ToInt32(Console.ReadLine());
+int Valx = x;
+
+x = y;
+y = Valx;
+Console.WriteLine(x + " " + y);
